d83319de605b2b7bef03ea5863e64fe145c1a1c1
Chirag Rajpal <chiragbrajpal@gmail.com>
2022-01-31 16:11:53 -0800

    Create SumtoRootLeaf.java

---
+++ SumtoRootLeaf.java
@@ -0,0 +1,27 @@
+//TC: O(H); given the amount of height of the BST;
+//SC: O(1) as no extra DS is used to return the sum other than a variable;
+
+class Solution {
+    int sum=0;
+    public int sumNumbers(TreeNode root) {
+        if(root==null) return 0;
+        helper(root,0);
+        //Calling helper function and the func does not end until manipulating the value of the gloabl varialble sum declared;
+        return sum;
+    }
+    private void helper(TreeNode root, int currSum){
+        //base case:
+        if(root==null){
+            return;
+        }
+        
+        if(root.left==null && root.right==null){
+            sum=sum+currSum*10+root.val;        //Comes here only when the traverse has reached to the leaf of the tree;
+            return;
+            //can write return here as well, as th ebase case of reaching the leaf will be there always;
+        }
+        helper(root.left,currSum*10+root.val);
+        helper(root.right,currSum*10+root.val);
+        
+    }
+}
